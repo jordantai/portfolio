4e2e7bcb3d6bdd2ebaf934cb5b796f7af1ba6d79
jordan <contact@creativecuckoo.com>
2020-09-01 22:21:14 +0100

    adds background images

---
+++ src/utils/background-images.js
@@ -0,0 +1,17 @@
+import arches from "../../content/assets/bgImg/arches.jpg"
+import beetham from "../../content/assets/bgImg/beetham.jpg"
+import chinaTown from "../../content/assets/bgImg/china-town.jpg"
+import deansgate from "../../content/assets/bgImg/deansgate.jpg"
+import nq from "../../content/assets/bgImg/nq.jpg"
+import street from "../../content/assets/bgImg/street.jpg"
+import townHall from "../../content/assets/bgImg/townhall.jpg"
+
+export const bgImgArray = [
+  arches,
+  beetham,
+  chinaTown,
+  deansgate,
+  nq,
+  street,
+  townHall,
+]
