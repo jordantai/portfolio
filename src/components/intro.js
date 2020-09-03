9b12eed36b430de369145dce747222e7d048d334
jordan <contact@creativecuckoo.com>
2020-09-03 13:33:18 +0100

    animates intro headers

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,38 +1,57 @@
 import React from "react"
 import { rhythm, scale } from "../utils/typography"
 import { GoMarkGithub } from "react-icons/go"
-import styled from "styled-components"
+import { zoomInRight, zoomInLeft, zoomInUp } from "react-animations"
+import styled, { keyframes, css } from "styled-components"
+
+const zoomInRightAnimation = keyframes`${zoomInRight} 0% {visibility: hidden} 100% {visibility: visible;}`
+const zoomInLeftAnimation = keyframes`${zoomInLeft} 0% {visibility: hidden} 100% {visibility: visible;}`
+const zoomInUpAnimation = keyframes`${zoomInUp} 0% {visibility: hidden} 100% {visibility: visible;}`
+
+const AnimatedHeader = styled.h2`
+  animation: ${props =>
+    props.right
+      ? css`2s ${zoomInRightAnimation}`
+      : css`2s ${zoomInLeftAnimation}`};
+  animation-delay: ${props => (props.right ? css`3s` : css`4s`)};
+  background: rgba(255, 255, 255, 0.8);
+  margin-bottom: ${rhythm(1)};
+  width: 70%;
+  text-align: center;
+  visibility: hidden;
+  animation-fill-mode: forwards;
+`
+const AnimatedP = styled.p`
+  animation: 2s ${zoomInUpAnimation};
+  visibility: hidden;
+  animation-fill-mode: forwards;
+  animation-delay: 5s;
+`
 
 const Intro = () => {
   return (
     <>
-      <h1
+      <AnimatedHeader
+        right
         style={{
-          background: `rgba(255, 255, 255, 0.8)`,
           ...scale(1),
-          marginBottom: rhythm(1),
-          width: `70%`,
           borderRadius: `1rem 0 1rem 1rem`,
-          textAlign: `center`,
           marginTop: 0,
           marginLeft: rhythm(1),
         }}
       >
         A Software Developer
-      </h1>
-      <h2
+      </AnimatedHeader>
+      <AnimatedHeader
         style={{
-          background: `rgba(255, 255, 255, 0.8)`,
           ...scale(0.6),
-          width: `70%`,
           marginLeft: rhythm(2),
           borderRadius: `0 1rem 1rem 1rem`,
-          textAlign: `center`,
         }}
       >
         based in Greater Manchester
-      </h2>
-      <p
+      </AnimatedHeader>
+      <AnimatedP
         style={{
           background: `rgba(255, 255, 255, 0.8)`,
           ...scale(0.2),
@@ -44,8 +63,10 @@ const Intro = () => {
         <a href="https://www.northcoders.com">Northcoders</a> Full Stack
         Developer Bootcamp and I am eager to hit the ground running and start a
         career in software development.
-      </p>
-      <GoMarkGithub />
+      </AnimatedP>
+      <a href="https://github.com/jordantai">
+        <GoMarkGithub size={`3rem`} color={`white`} />
+      </a>
     </>
   )
 }
