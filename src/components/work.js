6c3e0a185da3115cb1421ddbc0468ff2f44322c6
jordan <contact@creativecuckoo.com>
2020-09-08 15:38:04 +0100

    adds background images for project sections

--- src/components/work.js
+++ src/components/work.js
@@ -3,6 +3,9 @@ import { Link } from "gatsby"
 import { rhythm, scale } from "../utils/typography"
 import styled from "styled-components"
 import crimeStatsImg from "../../content/assets/crime-stats-mcr.png"
+import chickenTinderImg from "../../content/assets/chicken-tinder.png"
+import smanImg from "../../content/assets/sman.png"
+import KMAshworthImg from "../../content/assets/km-ashworth.png"
 
 const ProjectsWrapper = styled.div`
   display: grid;
@@ -12,9 +15,60 @@ const ProjectsWrapper = styled.div`
 `
 
 const ProjectBox = styled.div`
-  background-color: white;
-  border-radius: 1rem;
+  position: relative;
+  border-radius: 2px;
   min-height: 10rem;
+  box-shadow: 5px 5px 9px -2px rgba(0, 0, 0, 0.75);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &::before {
+    content: "";
+    background-size: cover;
+    position: absolute;
+    top: 0px;
+    right: 0px;
+    bottom: 0px;
+    left: 0px;
+    opacity: 0.4;
+  }
+  &:hover {
+    &::before {
+      opacity: 1;
+    }
+  }
+`
+const ProjectCrimeStats = styled(ProjectBox)`
+  &::before {
+    background-image: url(${crimeStatsImg});
+  }
+`
+
+const ProjectChickenTinder = styled(ProjectBox)`
+  &::before {
+    background-image: url(${chickenTinderImg});
+  }
+`
+
+const ProjectSMAN = styled(ProjectBox)`
+  &::before {
+    background-image: url(${smanImg});
+  }
+`
+
+const ProjectKMAshworth = styled(ProjectBox)`
+  &::before {
+    background-image: url(${KMAshworthImg});
+  }
+`
+
+const ProjectTitle = styled.h3`
+  z-index: 1;
+  background-color: rgba(65, 158, 218, 0.8);
+  padding: ${rhythm(1)};
+  margin-bottom: 0;
+  border-radius: 2px;
+  color: white;
 `
 
 const Work = () => {
@@ -22,23 +76,19 @@ const Work = () => {
     <>
       <h2>My Work</h2>
       <ProjectsWrapper>
-        <ProjectBox
-          style={{
-            backgroundImage: `url(${crimeStatsImg})`,
-            backgroundSize: `100%`,
-          }}
-        >
-          <h3>Crime Stats MCR</h3>
-        </ProjectBox>
-        <ProjectBox>
-          <h3>Chicken Tinder</h3>
-        </ProjectBox>
-        <ProjectBox>
-          <h3>Super Mega Awesome News</h3>
-        </ProjectBox>
-        <ProjectBox>
-          <h3>KM Ashworth Interiors</h3>
-        </ProjectBox>
+        <ProjectCrimeStats>
+          <ProjectTitle>Crime Stats MCR</ProjectTitle>
+          <h5>Tech</h5>
+        </ProjectCrimeStats>
+        <ProjectChickenTinder>
+          <ProjectTitle>Chicken Tinder</ProjectTitle>
+        </ProjectChickenTinder>
+        <ProjectSMAN>
+          <ProjectTitle>Super Mega Awesome News</ProjectTitle>
+        </ProjectSMAN>
+        <ProjectKMAshworth>
+          <ProjectTitle>KM Ashworth Interiors</ProjectTitle>
+        </ProjectKMAshworth>
       </ProjectsWrapper>
     </>
   )
