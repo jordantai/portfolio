fbab3cfba4f5c6423b41c1a30616b81fec2b3bbe
jordan <contact@creativecuckoo.com>
2020-09-10 23:10:19 +0100

    adds navbar, needs styling

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,13 +1,34 @@
 import React from "react"
+import styled from "styled-components"
+import { Link } from "gatsby"
+
+const NavLinks = styled.ul`
+  list-style: none;
+  margin: 0;
+  display: flex;
+  justify-content: flex-end;
+`
 
 const Navbar = () => {
   return (
     <nav>
-      <ul>
+      <NavLinks>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
         <li>
           <a href="#work">Work</a>
         </li>
-      </ul>
+        <li>
+          <a href="#skills">Skills</a>
+        </li>
+        <li>
+          <Link to="/blog">Blog</Link>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </NavLinks>
     </nav>
   )
 }
