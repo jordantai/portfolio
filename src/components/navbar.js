41b61e88008338a9809d461035bfce963b8ebf6a
jordan <contact@creativecuckoo.com>
2020-09-11 12:11:00 +0100

    completes navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,12 +1,25 @@
 import React from "react"
 import styled from "styled-components"
 import { Link } from "gatsby"
+import { rhythm } from "../utils/typography"
 
 const NavLinks = styled.ul`
   list-style: none;
   margin: 0;
   display: flex;
   justify-content: flex-end;
+  li {
+    background-color: rgba(65, 158, 218, 0.8);
+    margin: ${rhythm(0.2)};
+    padding: ${rhythm(0.2)};
+    border-radius: 4rem;
+    a {
+      color: white;
+    }
+    &:hover {
+      background-color: #2a6496;
+    }
+  }
 `
 
 const Navbar = () => {
